9a458f74f98267261164f287d7ebadf9d771afa3
koficypher <skcypher6@gmail.com>
2020-08-13 01:46:42 +0000

    partial customers feature added

--- src/Customer.php
+++ src/Customer.php
@@ -12,13 +12,26 @@ class Customer extends Executor {
     /**
      * Create a customer on an integration account
      *
-     * @param array $data - body field data according to api docs
-     * @param string $seckey
+     * @param array $data - body fields data according to api docs
+     * @param string $seckey - Paystack secret key
      * @return object - the response object
      */
     public function createCustomer(array $data, string $seckey = null)
     {
       return new Response($this->postRequest(Endpoint::Customer, $data, null, $seckey));
+    }
+
+
+    /**
+     * List all customers on an integration
+     *
+     * @param array $params - parameter fields according to the api docs
+     * @param string $seckey - 
+     * @return object - the response object
+     */
+    public function listCustomers(array $params = null, string $seckey = null)
+    {
+      return new Response($this->getRequest(Endpoint::Customer, null, $params, $seckey));
     }
     
 }
